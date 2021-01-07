608be5cf6497e75503c2696049cb908e0cb81e41
Arttu Hartikainen <arttuhartikainen@Arttu-MacBook-Pro.local>
2021-01-07 14:01:10 +0200

    Popup component and styling for popup and results field

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import Search from "./components/Search";
 import axios from "axios";
+import Results from "./components/Results";
+import Popup from "./components/Popup";
 
 function App() {
 	const [state, setState] = useState({
@@ -13,8 +15,11 @@ function App() {
 
 	const search = e => {
 		if (e.key === "Enter") {
-			axios(apiurl + "&s=" + state.searchQuery).then(data => {
-				console.log(data);
+			axios(apiurl + "&s=" + state.searchQuery).then(({ data }) => {
+				let results = data.Search;
+				setState(previousState => {
+					return { ...previousState, results: results };
+				});
 			});
 		}
 	};
@@ -27,13 +32,34 @@ function App() {
 		console.log(state.searchQuery);
 	};
 
+	const openPopup = id => {
+		axios(apiurl + "&i=" + id).then(({ data }) => {
+			let result = data;
+			setState(previousState => {
+				return { ...previousState, selected: result };
+			});
+		});
+	};
+
+	const closePopup = () => {
+		setState(previousState => {
+			return { ...previousState, selected: {} };
+		});
+	};
+
 	return (
 		<div className="App">
 			<header>
-				<h1>Works?</h1>
+				<h1>React Movie Database</h1>
 			</header>
 			<main>
 				<Search handleInput={handleInput} search={search} />
+				<Results results={state.results} openPopup={openPopup} />
+				{typeof state.selected.Title != "undefined" ? (
+					<Popup selected={state.selected} closePopup={closePopup} />
+				) : (
+					false
+				)}
 			</main>
 		</div>
 	);
